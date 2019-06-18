861c1423cb76a0ed779116af51492d12c9c11925
adadswelldev <51314790+adadswelldev@users.noreply.github.com>
2019-06-18 17:27:25 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,28 @@
+//Accesses the DOM elements
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+function runTheClock() {
+
+    //Set up date and gets individual values for hours, minutes and seconds
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let milli = date.getMilliseconds();
+
+    //Create and sets position variables
+    let hrPosition = (hr*360/12)+(min*(360/60)/12);
+    let minPosition = (min*360/60)+(sec*(360/60)/60);
+    let secPosition = sec*360/60;
+    
+    //Transforms the hands of the clock using CSS
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
+
