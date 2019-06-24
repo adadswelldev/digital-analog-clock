e925979250cdbebe980fdd39a9cdc05a77aa6a44
Adam Dadswell <AdamDadswell@rentalcars.com>
2019-06-24 08:35:14 +0100

    Added additional tests for DtoP

--- src/date-to-position.js
+++ src/date-to-position.js
@@ -2,7 +2,8 @@ module.exports = (date) => {
 
     var positions = {
         hoursPosition: date.getHours()*(360/12),
-        minutesPosition: date.getMinutes()*(360/60)
+        minutesPosition: date.getMinutes()*(360/60),
+        secondsPosition: date.getSeconds()*(360/60)
     };
 
     return positions;
